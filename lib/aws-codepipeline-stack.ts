4ad2a67509f1b68286d8d606ab938a4c8489e1f9
Sridhar Gnanasekaran <sridhar@surepay.nl>
2021-10-29 12:54:13 +0200

    Initial commit

--- lib/aws-codepipeline-stack.ts
+++ lib/aws-codepipeline-stack.ts
@@ -1,9 +1,21 @@
 import * as cdk from '@aws-cdk/core';
+import {PipelineAppStage} from "./pipeline-app-stage";
+import {CodePipeline, CodePipelineSource, ShellStep} from "@aws-cdk/pipelines";
 
 export class AwsCodepipelineStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        const pipeline = new CodePipeline(this, 'ExamplePipeline', {
+            pipelineName: 'ExamplePipeline',
+            synth: new ShellStep('Synth', {
+                input: CodePipelineSource.gitHub('srid99/aws-codepipeline', 'master'),
+                commands: ['npm ci', 'npm run build', 'npx cdk synth']
+            })
+        });
+
+        pipeline.addStage(new PipelineAppStage(this, "dummy-stage", {
+            env: {account: "012290904591", region: "eu-west-1"}
+        }));
+    }
 }
